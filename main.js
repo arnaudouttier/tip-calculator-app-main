9e98a8e50d8482147fbc07e3557e26aaa5293f38
arnaudouttier <arnoutti@gmail.com>
2022-04-22 17:30:04 +0200

    nlle master

--- main.js
+++ main.js
@@ -1,81 +1,74 @@
+const calculator = document.querySelector(".calculator");
+const calculatorPeopleBLock = document.querySelector(".calculator-people");
 const billAmount = document.querySelector("#bill-amount");
-const customTip = document.querySelector("#custom-tip");
-const numberPeopleGroup = document.querySelector(".calculator-people");
-const spanWarning = document.querySelector(".span-warning");
-const nbOfPeople = document.querySelector("#people-number");
-const tipResult = document.querySelector(
+const percentageTip = document.querySelector(".bill");
+const nfPeople = document.querySelector("#people-number");
+
+const calculatorReset = document.querySelector(".calculator-reset");
+
+const resultTipTt = document.querySelector(
   ".calculator-tip-amount .result-total"
 );
-const personResult = document.querySelector(".calculator-total .result-total");
+const resultBillTt = document.querySelector(".calculator-total .result-total");
 
-let np = 0;
-var percentageButton = 0;
+let bill = 0;
+let tipPerc = 0;
+let numberCLient = 0;
 
-const convertToNumber = (x, base) => {
-  const parsed = parseFloat(x, base);
-  if (isNaN(parsed)) {
-    return 0;
+const tipAmountTotal = (billentage, percTip, peopleN) => {
+  return (bill * (percTip / 100)) / peopleN;
+};
+
+const billTotal = (bill, peopleN, percTip) => {
+  return bill / peopleN + percTip;
+};
+
+const formatView = (value) => {
+  if (isNaN(value) || value === Infinity) {
+    value = "$ 0.00";
   }
-  return parsed;
-};
-
-const tipAmount = (billTotal, percentageTip, numberPeople) => {
-  let result = (billTotal * percentageTip) / 100 / numberPeople;
-  return convertToNumber(result, 10);
-};
-
-const totalPerson = (billTotal, numberPeople, amountTip) => {
-  let result = billTotal / numberPeople + amountTip;
-  return convertToNumber(result, 10);
-};
-
-const peopleInputRender = (inputSelector) => {
-  if (inputSelector.value == 0) {
-    numberPeopleGroup.classList.add("warning");
-    spanWarning.style.display = "block";
-  } else {
-    numberPeopleGroup.classList.remove("warning");
-    spanWarning.style.display = "none";
-  }
-};
-
-const renderResultBill = (aTip, ttPerson) => {
-  tipResult.innerHTML = `$ ${aTip}`;
-  if (ttPerson === Infinity) {
-    personResult.innerHTML = `$ 0.00`;
-  } else {
-    personResult.innerHTML = `$ ${ttPerson}`;
-  }
+  return value;
 };
 
 const reset = () => {
   billAmount.value = "";
-  customTip.value = "";
-  nbOfPeople.value = "";
-  tipResult.innerHTML = `$ 0.00`;
-  personResult.innerHTML = `$ 0.00`;
+  nfPeople.value = "1";
+  bill = 0;
+  tipPerc = 0;
+  numberCLient = 0;
+  resultTipTt.innerHTML = "$ 0.00";
+  resultBillTt.innerHTML = "$ 0.00";
 };
 
-const calculateTip = () => {
-  let bill = document.querySelector("#bill-amount").value;
-  let nP = nbOfPeople.value;
+const changeOnCalculator = (event) => {
+  if (event.target.name === "bill") {
+    bill = event.target.value;
+  }
 
-  document.querySelectorAll(".percentage-btn").forEach((btn) => {
-    btn.addEventListener("click", (event) => {
-      renderResultBill(
-        tipAmount(bill, event.currentTarget.value, nP),
-        totalPerson(bill, nP, tipAmount(bill, event.currentTarget.value, nP))
-      );
-    });
-  });
+  if (event.target.name === "client") {
+    numberCLient = event.target.value;
+    console.log(numberCLient);
 
-  peopleInputRender(nbOfPeople);
+    if (numberCLient === "0") {
+      calculatorPeopleBLock.classList.add("warning");
+    } else {
+      calculatorPeopleBLock.classList.remove("warning");
+    }
+  }
 
-  renderResultBill(
-    tipAmount(bill, customTip.value, nP),
-    totalPerson(bill, nP, tipAmount(bill, customTip.value, nP))
+  if (event.target.type === "radio" || event.target.name === "custom-perc") {
+    tipPerc = event.target.value;
+    console.log(tipPerc);
+  }
+
+  resultTipTt.innerHTML = formatView(
+    tipAmountTotal(bill, tipPerc, numberCLient)
+  );
+  resultBillTt.innerHTML = formatView(
+    billTotal(bill, numberCLient, tipAmountTotal(bill, tipPerc, numberCLient))
   );
 };
 
-document.querySelector(".calculator").addEventListener("change", calculateTip);
-document.querySelector(".calculator-reset").addEventListener("click", reset);
+calculator.addEventListener("change", changeOnCalculator);
+
+calculatorReset.addEventListener("click", reset);
